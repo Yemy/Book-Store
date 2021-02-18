a730b95124886f7cfa6c2095bad4e7a28258f426
yemi <yemybold@gmail.com>
2021-02-18 20:32:23 +0300

    ch9 email provider not yet and reset and change not yet done

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -179,3 +179,6 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+
+# default from email  
+DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
